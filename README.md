d27f07104037c88f23c7489258218b8fef87f543
daniellima <daniellima.pessoal@gmail.com>
2015-09-24 22:39:45 -0300

    Adicionando as dependencias do bower

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # angulist
-A projet used to learn angular. The main objective is a simple todolist written using Angular.js and Laravel 5
+A project used to learn angular. The main objective is a simple todolist written using Angular.js and Laravel 5
 
 The objective is to have a pure javascript frontend and a Laravel Backed API. I will try to adhere to the RESTfull pattern.
 
-Inspiration has come from todomvc.com and the fact I love todo lists.
+Inspiration has come from todomvc.com and the fact that I love todo lists.
 
 Features:
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Angulist</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,8 +38,14 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Angulist</div>
             </div>
         </div>
+
+        <script src="{{ asset("bower_components/angular/angular.js") }}"></script>
+        <script src="{{ asset("bower_components/angular-route/angular-route.js") }}"></script>
+        <script src="{{ asset("bower_components/angular-resource/angular-resource.js") }}"></script>
+        <script src="{{ asset("bower_components/angular-animate/angular-animate.js") }}"></script>
+        <script src="{{ asset("bower_components/angular-messages/angular-messages.min.js") }}"></script>
     </body>
 </html>
